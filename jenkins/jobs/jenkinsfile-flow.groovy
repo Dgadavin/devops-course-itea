6794602f2813bd66c3aebfac572ee172681d6b1d
Sotnikov Maksym <maxferiartos@gmail.com>
2019-06-25 21:44:52 +0300

    Create jenkinsfile-flow.groovy

---
+++ jenkins/jobs/jenkinsfile-flow.groovy
@@ -0,0 +1,29 @@
+pipelineJob("test-pipeline") {
+    	logRotator {
+       		numToKeep(10)
+    	}
+
+    parameters {
+      choiceParam('ACTION', ["plan", "apply"], "Choose current action")
+      stringParam('COMMIT_HASH')
+	  booleanParam('BUILD_IMAGE', true, 'Build image')
+    }
+
+    definition {
+        cpsScm {
+   			scm {
+            	git {
+                  remote {
+                        github("dgadavin/devops-course-itea", 'http')
+                        branch("\master")
+                  }
+              }
+        }
+        scriptPath("jenkins/Jenkinsfile")
+      }
+
+    }
+
+ }
+
+}
