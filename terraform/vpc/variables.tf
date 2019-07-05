c70bd0d61dd928bab63b282b7ac6b694a4569d34
Sotnikov Maksym <maxferiartos@gmail.com>
2019-07-05 11:15:00 +0300

    Add vars to VPC

--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -3,4 +3,6 @@ variable "public_subnet_cidr" {default = "10.0.0.0/24"}
 variable "private_subnet_cidr" {default = "10.0.10.0/24"}
 variable "region" { default = "eu-west-1" }
 variable "short_name" { default = "itea-test" }
-variable "profile_name" { default = "glomex" }
+variable "profile_name" { default = "glomex" } //only for instructor
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
