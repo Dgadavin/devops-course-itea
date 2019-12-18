f4e8fe80a4ea43c67ae7a72cbfdbbf617017e491
Sotnikov Maksym <maxferiartos@gmail.com>
2019-12-18 11:09:16 +0200

    Fix

--- advance_course/terraform/ecs-service/task-definition.tf
+++ advance_course/terraform/ecs-service/task-definition.tf
@@ -2,9 +2,9 @@ data "template_file" "nginxTemplate" {
   template = "${file("./templates/task_definition.json")}"
 
   vars = {
-    app_env = "${var.Environment}"
+    app_env           = "${var.Environment}"
     app_internal_host = "${var.ELBDNSName}.${var.HostedZone}"
-    docker_image = "nginx:${var.commit_hash}"
-    service_name = "${var.service_name}"
+    docker_image      = "nginx:${var.commit_hash}"
+    service_name      = "${var.service_name}"
   }
 }
