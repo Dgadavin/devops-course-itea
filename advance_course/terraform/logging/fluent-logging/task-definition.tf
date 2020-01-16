a484565328a713f49e6567a5d7d964c40369bb37
Sotnikov Maksym <maxferiartos@gmail.com>
2020-01-16 13:51:42 +0200

    Fix

--- advance_course/terraform/logging/fluent-logging/task-definition.tf
+++ advance_course/terraform/logging/fluent-logging/task-definition.tf
@@ -3,8 +3,8 @@ data "template_file" "FluentServiceTemplate" {
 
   vars {
     aws_region   = "us-east-1"
-    docker_image = ""
+    docker_image = "644239850139.dkr.ecr.us-east-1.amazonaws.com/fluentd:1.0"
     service_name = "${var.service_name}"
-    elk_url      = ""
+    elk_url      = "vpc-tf-logging-elastic-dev-cfdts3bsqszdnh3yj4nbpdqrem.us-east-1.es.amazonaws.com"
   }
 }
